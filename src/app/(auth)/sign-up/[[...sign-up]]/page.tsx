afee0e3df8a787c04ea4a13a3d370abbc0b06030
Vedant Chainani <vedantchainani1084@gmail.com>
2024-02-15 19:37:43 +0530

    fix: footer on signup page and update breakpoint to xl

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -6,23 +6,22 @@ import { SatyaNadellaImage } from '~/assets';
 
 const SignUpPage = () => {
   return (
-    <div className='flex min-h-screen w-full flex-col lg:flex-row'>
-      <div className='h-[40dvh] w-full bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] from-white via-[#007DFF] to-[#007DFF] lg:min-h-screen lg:basis-1/3' />
+    <div className='flex min-h-screen w-full flex-col xl:flex-row'>
+      <div className='h-[40dvh] w-full bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] from-white via-[#007DFF] to-[#007DFF] xl:min-h-screen xl:basis-1/3' />
 
-      <div className='flex w-full flex-col items-center justify-between lg:basis-2/3 lg:flex-row'>
+      <div className='flex w-full flex-col items-center justify-evenly xl:basis-2/3 xl:flex-row'>
         <SignUp
           redirectUrl='/dashboard'
           appearance={{
             elements: {
               formButtonPrimary: 'bg-primary hover:bg-secondary',
               footerActionLink: 'text-secondary hover:text-primary',
-              footerAction: 'hidden',
-              card: 'mx-0 w-[22rem] gap-2 sm:w-[28rem] sm:gap-8 lg:-translate-x-1/2 translate-x-0 -translate-y-1/2 lg:translate-y-0 sm:h-[40rem]',
+              card: 'mx-0 w-[22rem] gap-2 sm:w-[28rem] sm:gap-8 xl:-translate-x-1/2 translate-x-0 -translate-y-1/2 xl:translate-y-0 sm:h-[40rem]',
             },
           }}
         />
 
-        <div className='flex w-full max-w-3xl -translate-y-2/3 flex-col gap-6 px-3 text-center text-xl text-slate-500 lg:-translate-y-0'>
+        <div className='flex w-full max-w-3xl -translate-y-2/3 flex-col gap-6 px-3 text-center text-xl text-slate-500 lg:mr-[4rem] lg:-translate-y-0'>
           <p>
             {`"The power of technology lies not in its complexity, but in its ability to simplify and empower. Let your innovations be a testament to the elegance of simplicity."`}
           </p>
@@ -39,9 +38,6 @@ const SignUpPage = () => {
               <p className='text-sm'>CEO, Microsoft</p>
             </div>
           </div>
-        </div>
-        <div className='hidden w-full max-w-[10rem] text-transparent lg:flex'>
-          a
         </div>
       </div>
     </div>
